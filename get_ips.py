3e35d62d90cc5c7313d590b2518fbc7051bb1cb1
gsheppar <gsheppar@gmail.com>
2026-01-28 08:26:36 -0800

    Add files via upload

--- get_ips.py
+++ get_ips.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
 
-# 20201020 - Add a function to add a single prefix to a local prefixlist - Dan
-import cloudgenix
+import prisma_sase
 import argparse
-from cloudgenix import jd, jd_detailed
-import cloudgenix_settings
+from prisma_sase import jd, jd_detailed, jdout
+import prismasase_settings
 import sys
 import logging
 import os
 import datetime
 import collections
-import csv 
-
-
+import csv
+import ipaddress
+from csv import DictReader
+import time
+from datetime import datetime, timedelta
+import math
+import re
 
 # Global Vars
 TIME_BETWEEN_API_UPDATES = 60       # seconds
 REFRESH_LOGIN_TOKEN_INTERVAL = 7    # hours
-SDK_VERSION = cloudgenix.version
-SCRIPT_NAME = 'CloudGenix: Example script: Get IPs'
+SCRIPT_NAME = 'CloudGenix: Get Public IP'
 SCRIPT_VERSION = "v1"
 
 # Set NON-SYSLOG logging to use function name
@@ -30,26 +32,14 @@ logger = logging.getLogger(__name__)
 ####################################################################
 
 sys.path.append(os.getcwd())
-try:
-    from cloudgenix_settings import CLOUDGENIX_AUTH_TOKEN
-
-except ImportError:
-    # Get AUTH_TOKEN/X_AUTH_TOKEN from env variable, if it exists. X_AUTH_TOKEN takes priority.
-    if "X_AUTH_TOKEN" in os.environ:
-        CLOUDGENIX_AUTH_TOKEN = os.environ.get('X_AUTH_TOKEN')
-    elif "AUTH_TOKEN" in os.environ:
-        CLOUDGENIX_AUTH_TOKEN = os.environ.get('AUTH_TOKEN')
-    else:
-        # not set
-        CLOUDGENIX_AUTH_TOKEN = None
 
 try:
-    from cloudgenix_settings import CLOUDGENIX_USER, CLOUDGENIX_PASSWORD
+    from prismasase_settings import PRISMASASE_CLIENT_ID, PRISMASASE_CLIENT_SECRET, PRISMASASE_TSG_ID
 
 except ImportError:
-    # will get caught below
-    CLOUDGENIX_USER = None
-    CLOUDGENIX_PASSWORD = None
+    PRISMASASE_CLIENT_ID=None
+    PRISMASASE_CLIENT_SECRET=None
+    PRISMASASE_TSG_ID=None
 
 def get(cgx):
     
@@ -123,90 +113,22 @@ def go():
     # Begin Script, parse arguments.
     ############################################################################
 
-    # Parse arguments
-    parser = argparse.ArgumentParser(description="{0}.".format(SCRIPT_NAME))
-
-    # Allow Controller modification and debug level sets.
-    controller_group = parser.add_argument_group('API', 'These options change how this program connects to the API.')
-    controller_group.add_argument("--controller", "-C",
-                                  help="Controller URI, ex. "
-                                       "Alpha: https://api-alpha.elcapitan.cloudgenix.com"
-                                       "C-Prod: https://api.elcapitan.cloudgenix.com",
-                                  default=None)
-    controller_group.add_argument("--insecure", "-I", help="Disable SSL certificate and hostname verification",
-                                  dest='verify', action='store_false', default=True)
-    login_group = parser.add_argument_group('Login', 'These options allow skipping of interactive login')
-    login_group.add_argument("--email", "-E", help="Use this email as User Name instead of prompting",
-                             default=None)
-    login_group.add_argument("--pass", "-PW", help="Use this Password instead of prompting",
-                             default=None)
-    debug_group = parser.add_argument_group('Debug', 'These options enable debugging output')
-    debug_group.add_argument("--debug", "-D", help="Verbose Debug info, levels 0-2", type=int,
-                             default=0)
+    sase_session = prisma_sase.API()
+    #sase_session.set_debug(2)
     
-    args = vars(parser.parse_args())
-                             
-    ############################################################################
-    # Instantiate API
-    ############################################################################
-    cgx_session = cloudgenix.API(controller=args["controller"], ssl_verify=args["verify"])
-
-    # set debug
-    cgx_session.set_debug(args["debug"])
-
-    ##
-    # ##########################################################################
-    # Draw Interactive login banner, run interactive login including args above.
-    ############################################################################
-    print("{0} v{1} ({2})\n".format(SCRIPT_NAME, SCRIPT_VERSION, cgx_session.controller))
-
-    # login logic. Use cmdline if set, use AUTH_TOKEN next, finally user/pass from config file, then prompt.
-    # figure out user
-    if args["email"]:
-        user_email = args["email"]
-    elif CLOUDGENIX_USER:
-        user_email = CLOUDGENIX_USER
-    else:
-        user_email = None
-
-    # figure out password
-    if args["pass"]:
-        user_password = args["pass"]
-    elif CLOUDGENIX_PASSWORD:
-        user_password = CLOUDGENIX_PASSWORD
-    else:
-        user_password = None
-
-    # check for token
-    if CLOUDGENIX_AUTH_TOKEN and not args["email"] and not args["pass"]:
-        cgx_session.interactive.use_token(CLOUDGENIX_AUTH_TOKEN)
-        if cgx_session.tenant_id is None:
-            print("AUTH_TOKEN login failure, please check token.")
-            sys.exit()
-
-    else:
-        while cgx_session.tenant_id is None:
-            cgx_session.interactive.login(user_email, user_password)
-            # clear after one failed login, force relogin.
-            if not cgx_session.tenant_id:
-                user_email = None
-                user_password = None
+    sase_session.interactive.login_secret(client_id=PRISMASASE_CLIENT_ID,
+                                          client_secret=PRISMASASE_CLIENT_SECRET,
+                                          tsg_id=PRISMASASE_TSG_ID)
 
     ############################################################################
     # End Login handling, begin script..
     ############################################################################
 
-    # get time now.
-    curtime_str = datetime.datetime.utcnow().strftime('%Y-%m-%d-%H-%M-%S')
-
-    # create file-system friendly tenant str.
-    tenant_str = "".join(x for x in cgx_session.tenant_name if x.isalnum()).lower()
-    cgx = cgx_session
+    cgx = sase_session
 
     get(cgx)
     
     # end of script, run logout to clear session.
-    cgx_session.get.logout()
 
 if __name__ == "__main__":
     go()
